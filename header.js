d2d9187b1121a24fdf8e42d68f100486426c2182
Evla <belzedark@live.fr>
2017-07-17 23:15:29 +0200

    SIEG MEIN KAISER

--- header.js
+++ header.js
@@ -3,11 +3,11 @@ document.write('\
 \			<h1> Le Shabbat avec Samuel /!\ En construction, titre provisoire /!\</h1>\
 \			<img src=\"images\/banniere.jpg\" alt=\"étendard\" id=\"banniere\"></a>\
 \			<nav>\
-\				<a href=\"enquete.html\"> Journal d\'une chasse sportive</a>\
-\				<a href=\"alice.html\"> Alice <3</a>\
-\				<a href=\"couture.html\"> Couture</a>\
-\				<a href=\"surprise.html\"> Surprise ? et random</a>\
-\				<a href=\"proj.html\"> Proj\'</a>\
-\				<a href=\"projet_vn.html\"> VN </a>\
+\				<a href=\"reinhard.png\"> Sieg mein Kaiser </a>\
+\				<--<a href=\"alice.html\"> Alice <3</a>-->\
+\				<--<a href=\"couture.html\"> Couture</a>-->\
+\				<--<a href=\"surprise.html\"> Surprise ? et random</a>-->\
+\				<--<a href=\"proj.html\"> Proj\'</a>-->\
+\				<--<a href=\"projet_vn.html\"> VN </a>-->\
 \			</nav>\
 \		</header>');
