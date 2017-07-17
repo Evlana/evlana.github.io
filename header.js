279a5e9663746d094438dc9e93e05b2d80f16e66
Evlana Pâlesang <Evlana@users.noreply.github.com>
2017-07-17 21:59:36 +0200

    Create header.js

---
+++ header.js
@@ -0,0 +1,18 @@
+document.write('\
+\<head>\
+\		<link rel="stylesheet" href="index.css" type="text/css">\
+\			<meta charset="UTF-8"/>\
+\			<title> L'Inquisition à l'ENSIIE</title>\
+\</head>\
+\		<header>\
+\			<h1> Le Shabbat avec Samuel /!\ En construction, titre provisoire /!\</h1>\
+\			<img src="banniere.jpg" alt="étendard" id="banniere"></a>\
+\			<nav>\
+\				<a href="enquete.html"> Journal d'une chasse sportive</a>\
+\				<a href="alice.html"> Alice <3</a>\
+\				<a href="couture.html"> Couture</a>\
+\				<a href="surprise.html"> Surprise ? et random</a>\
+\				<a href="proj.html"> Proj'</a>\
+\				<a href="projet_vn.html"> VN </a>\
+\			</nav>\
+\		</header>');
