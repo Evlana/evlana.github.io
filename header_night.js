5b30b7e3ff05efe0a68a0c226827280c21d5e187
root <root@Pc.rd.francetelecom.fr>
2017-07-18 15:51:25 +0200

    back

---
+++ header_night.js
@@ -0,0 +1,13 @@
+document.write('\
+\		<header>\
+\			<h1> Cauchemar du chasseur /!\ En construction, titre provisoire /!\</h1>\
+\			<img src=\"images\/banniere_nuit.jpg\" alt=\"étendard\" id=\"banniere\"></a>\
+\			<nav>\
+\				<a href=\"images/reinhard.png\"> Sieg mein Kaiser </a>\
+\				<\!--<a href=\"alice.html\"> Alice <3</a>-->\
+\				<\!--<a href=\"couture.html\"> Couture</a>-->\
+\				<\!--<a href=\"surprise.html\"> Surprise ? et random</a>-->\
+\				<\!--<a href=\"proj.html\"> Proj\'</a>-->\
+\				<\!--<a href=\"projet_vn.html\"> VN </a>-->\
+\			</nav>\
+\		</header>');
